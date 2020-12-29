5f0c5fac16c37a9ca95260e3053a089c4e3053ce
Everett Diuguid <everett.diuguid@gmail.com>
2020-12-29 10:01:12 -0500

    card deck example loaded

---
+++ index.js
@@ -0,0 +1,25 @@
+var $container = document.getElementById('container');
+
+// create Deck
+var deck = Deck();
+
+// add to DOM
+deck.mount($container);
+
+deck.shuffle();
+deck.cards.forEach(function (card, i) {
+    card.setSide('back');
+    card.enableDragging();
+    card.enableFlipping();
+    
+
+    // // explode
+    // card.animateTo({
+    //     delay: 1000 + i * 2, // wait 1 second + i * 2 ms
+    //     duration: 5000,
+    //     ease: 'quartOut',
+
+    //     x: Math.random() * window.innerWidth - window.innerWidth / 2,
+    //     y: Math.random() * window.innerHeight - window.innerHeight / 2
+    // });
+});
